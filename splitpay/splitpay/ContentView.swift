6c251a063b6e65170294d61c13bce5bd9d04e927
saheb9631 <92590505+saheb9631@users.noreply.github.com>
2026-01-31 11:02:44 +0530

    first_commit

---
+++ splitpay/splitpay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  splitpay
+//
+//  Created by Eazydiner on 31/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ splitpay/splitpay/splitpayApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitpayApp.swift
+//  splitpay
+//
+//  Created by Eazydiner on 31/01/26.
+//
+
+import SwiftUI
+
+@main
+struct splitpayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
